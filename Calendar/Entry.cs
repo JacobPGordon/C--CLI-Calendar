f4646eef62588f6e592aef2f14e21d56faeb5fc9
Hazeorn <jkegordon6@gmail.com>
2023-09-05 15:35:57 -0400

    first commit

---
+++ Calendar/Entry.cs
@@ -0,0 +1,14 @@
+
+using System.ComponentModel;
+
+public class Entry{
+    //A datetime representation of the date entered for this item
+    DateTime date;
+    //The actual text of the item
+    String? desc;
+    //The number of minutes that the program should wait for to repeat its warning
+    int? repeat;
+    
+
+
+}
---
+++ Calendar/Program.cs
@@ -0,0 +1,34 @@
+﻿string DFILE = "./dates_file";
+Entry[] dates;
+
+void add_date(){
+
+}
+
+void remove_date(){
+
+}
+
+
+//Fetch dates from the file upon load
+void load_dates(){
+    //Using streamreader we can read the file line by line easily
+    using (StreamReader sr = new StreamReader(DFILE)){
+        while (sr.Peek() >= 0){
+            string currentDate = sr.ReadLine();
+            string[] split = currentDate.Split(";");
+        }
+    }
+}
+
+
+//Creating data file if it doesn't yet exist
+if(!File.Exists(DFILE)){
+    
+    using (FileStream fs = File.Create(DFILE));
+
+}
+
+load_dates();
+
+
